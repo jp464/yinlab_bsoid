0fd112dcdb3ceb2b4189caa8ab64f771dc9b5752
JongYup Park <jongyup.park@duke.edu>
2022-05-04 22:34:54 -0400

    filter code

---
+++ filter.py
@@ -0,0 +1,23 @@
+# ===================================================================================
+# SUMMARY OF CODE
+# Filters for given next transition label 
+
+# PROCEDURE
+# Modify the FILE to be the path of the file you wish to analyze. The DEST 
+# is where the result will be stored in. You can also change the name of the file. 
+# In the terminal, change the directory to where the program is located and enter
+# "python filter.py". 
+# ===================================================================================
+
+import pandas as pd 
+
+FILE = "/Users/stan.park712/Library/CloudStorage/Box-Box/jp464/yinLab/yinlab_bsoid/test_data/frame_transitions.csv"
+DEST = "/Users/stan.park712/Library/CloudStorage/Box-Box/jp464/yinLab/yinlab_bsoid/test_data"
+DEST += "/filtered_frame_transitions.csv"
+df = pd.read_csv(FILE)
+
+label = int(input("Enter label to filter for"))
+
+
+df = df[df["next_label"] == label]
+df.to_csv(DEST)
